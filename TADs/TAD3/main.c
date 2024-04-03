67418abd940b396b8625791036ad2bcc1315566f
Italo Reis <italohenrikbr@gmail.com>
2024-04-03 12:01:38 -0300

    Adjusting

--- TADs/TAD3/main.c
+++ TADs/TAD3/main.c
@@ -6,21 +6,28 @@ int main() {
 
     Conjunto *c1 = criarConjunto();
 
-    inserirElemento(c1, 1);
+    /* inserirElemento(c1, 1);
     inserirElemento(c1, 2);
     inserirElemento(c1, 3);
-    inserirElemento(c1, 4);
+    inserirElemento(c1, 4); */
 
     Conjunto *c2 = criarConjunto();
 
-    inserirElemento(c2, 3);
-    inserirElemento(c2, 4);
     inserirElemento(c2, 5);
-    inserirElemento(c2, 6);
+    inserirElemento(c2, 3);
+    inserirElemento(c2, 7);
+    inserirElemento(c2, 4);
 
-    Conjunto *c3 = diferenca(c1, c2);
+    //maior valor
+    printf("O maior valor do conjunto c2 e: %d\n", maiorValor(c2));
 
-    printConjunto(c3);
+    //menor valor
+    printf("O menor valor do conjunto c2 e: %d\n", menorValor(c2));
+
+    printf("O conjunto c1 e vazio ? --> %s", conjuntoVazio(c1) ? "Conjunto vazio" : "Conjunto nao vazio");
+
+    printf("\nos conjuntos sao iguais ? --> %s", verificarIgualdade(c1, c2) ? "sao iguais" : "nao sao iguais");
+
 
 
     return 0;
