21f1d5b4cbec5d97c3c1b3b9db153c445935478f
Italo Reis <italohenrikbr@gmail.com>
2024-05-27 16:36:48 -0300

    current

--- pilhas/pilhas_estaticas/palindromo/main.c
+++ pilhas/pilhas_estaticas/palindromo/main.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 bool verificar_palindromo(Stack *stack, char *palavra) {
-    int len = strlen(palavra);
     char value = 0;
 
-    for (int i = 0; i < strlen(palavra); i++) {
+    int i;
+    for (i = 0; i < strlen(palavra); i++) {
         Stack_pop(stack, &value);
         if (value != palavra[i]) {
             return false;
@@ -18,17 +18,16 @@ bool verificar_palindromo(Stack *stack, char *palavra) {
 
 int main() {
 
-    char *str = "bob";
-
+    char *str = "mirim";
     Stack *stack = Stack_create(100);
+
     int i;
-    int aux = strlen(str);
-    
     for (i = 0; i < strlen(str); i++) {
         Stack_push(stack, str[i]);
     }
 
-    printf("\ne um palindromo ? %d\n", verificar_palindromo(stack, str));
+    puts("");
+    printf("e um palindromo ? %d\n", verificar_palindromo(stack, str));
 
     return 0;
 }
